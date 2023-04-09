b1fb24099784f0bfba99cfba5d13ad7adf34721e
Enes1864 <117340974+Enes1864@users.noreply.github.com>
2023-04-10 00:27:49 +0300

    enes branchi 2.degisiklik

--- src/test/java/day_02_BranchCalismasi/Enes_Branchim.java
+++ src/test/java/day_02_BranchCalismasi/Enes_Branchim.java
@@ -4,5 +4,6 @@ public class Enes_Branchim {
     public static void main(String[] args) {
         System.out.println("Enes Branchini calisiyorum.");
         //ben degisiklik yaptim ve bunu kalici hale getirmek icin commit yaptim.
+        System.out.println("Enes Branchinde 2.degisiklik kendime ozel classtsa");
     }
 }
---
+++ src/test/java/day_02_BranchCalismasi/Ortak_Class.java
@@ -0,0 +1,7 @@
+package day_02_BranchCalismasi;
+
+public class Ortak_Class {
+    public static void main(String[] args) {
+        System.out.println("Enes Branchinde ortak classda degisiklik yaptim");
+    }
+}
