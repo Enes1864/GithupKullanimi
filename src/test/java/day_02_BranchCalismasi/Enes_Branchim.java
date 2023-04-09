5015a7413d6f113d4db364e240350dc29b1dc082
Enes1864 <117340974+Enes1864@users.noreply.github.com>
2023-04-10 00:14:38 +0300

    Enes branchini guncelledim

---
+++ src/test/java/day_02_BranchCalismasi/Enes_Branchim.java
@@ -0,0 +1,8 @@
+package day_02_BranchCalismasi;
+
+public class Enes_Branchim {
+    public static void main(String[] args) {
+        System.out.println("Enes Branchini calisiyorum.");
+        //ben degisiklik yaptim ve bunu kalici hale getirmek icin commit yaptim.
+    }
+}
