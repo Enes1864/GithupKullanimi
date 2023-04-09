87248e86d4331c1f6876b11ff195ac44e024c47e
Enes1864 <117340974+Enes1864@users.noreply.github.com>
2023-04-10 00:37:02 +0300

    enes brancim desisti

--- src/test/java/day_02_BranchCalismasi/Ortak_Class.java
+++ src/test/java/day_02_BranchCalismasi/Ortak_Class.java
@@ -2,6 +2,7 @@ package day_02_BranchCalismasi;
 
 public class Ortak_Class {
     public static void main(String[] args) {
-        System.out.println("Enes Branchinde ortak classda degisiklik yaptim");
+
+        System.out.println("Enes Branchinde ortak classda ilk kodu yazdim");
     }
 }
